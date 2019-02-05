276be77b03c8ae82bac21dbd0b7dbc46e5f7d6e5
Tanguy Busschaert <busschaert.tanguy@hotmail.com>
2019-02-05 07:23:41 +0100

    refactor repo

---
+++ src/Account.Domain/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Core.Domain;
+
+namespace Account.Domain
+{
+    public interface IAccountRepository
+    {
+        Task<Account> GetByUserName(string username);
+    }
+}
